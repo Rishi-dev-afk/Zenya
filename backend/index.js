f53589889b70dabf9833bda9490840e3f05dbd9d
DibyaJyoti Parida <dibyajyotiparida940@gmail.com>
2025-04-04 19:06:12 +0530

    add new api endpoint

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,16 @@ app.use(express.json());
 const studentSign = require("./routes/studentsign");
 const facultySign = require("./routes/facultysign");
 const adminSign = require("./routes/adminsign");
+const studentLogin = require("./routes/studentlogin");
+const facultyLogin = require("./routes/facultylogin");  
+const adminLogin = require("./routes/adminlogin");
 
 app.use("/api", studentSign);
 app.use("/api", facultySign);
 app.use("/api", adminSign);
+app.use("/api", studentLogin);
+app.use("/api", facultyLogin);
+app.use("/api", adminLogin);
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
